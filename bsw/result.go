80ff91634d26e81f4b893bda637ecb2407d61530
tomsteele <thomasjsteele@gmail.com>
2013-12-03 16:25:54 -0800

    Provides packge comment

--- bsw/result.go
+++ bsw/result.go
@@ -1,8 +1,11 @@
+/*Package contains result type definitions and functions that are used by the main
+  tool 'blacksheepwall'*/
+
 package bsw
 
 import (
-	"net"
 	"encoding/binary"
+	"net"
 )
 
 type Result struct {
@@ -13,7 +16,7 @@ type Result struct {
 
 type Results []Result
 
-func (r Results) Len() int { return len(r) }
+func (r Results) Len() int      { return len(r) }
 func (r Results) Swap(i, j int) { r[i], r[j] = r[j], r[i] }
 
 // Sorts by IPv4 address, IPv6 addresses will be show first and will be unsorted.
