51711de2955b027d6bbe20edb2c8546619e08075
Tom Steele <thomasjsteele@gmail.com>
2015-03-23 11:04:58 -0700

    Fix dictionary tests

--- bsw/dictionary_test.go
+++ bsw/dictionary_test.go
@@ -5,21 +5,38 @@ import (
 )
 
 func TestWildCard(t *testing.T) {
-	ip := GetWildCard("huptwo34.com", "208.67.222.222")
+	ip := GetWildCard("stacktitan.com", "8.8.8.8")
 	if ip == "" {
 		t.Error("Failed to get A record for wildcard")
 	}
 }
 
-func testDictionary(t *testing.T) {
-	results, _ := Dictionary("huptwo34.com", "www", "", "8.8.8.8")
-	if results[0].IP != "209.147.121.178" {
+func TestDictionary(t *testing.T) {
+	_, results, _ := Dictionary("stacktitan.com", "foo", "", "8.8.8.8")
+	if len(results) < 1 {
+		t.Fatal("Dictionary did not return any results")
+	}
+	if results[0].IP != "104.131.56.170" {
 		t.Error("Dictionary returned incorrect or non-existent IP Address")
 	}
-	if results[0].Hostname != "www.huptwo34.com" {
-		t.Error("Dictioanry returned incorrect hostname")
+	if results[0].Hostname != "foo.stacktitan.com" {
+		t.Error("Dictionary returned incorrect hostname")
 	}
-	if results[0].Source != "Dictionary" {
+	if results[0].Source != "Dictionary IPv4" {
+		t.Error("Dictionary returned incorrect source")
+	}
+
+	_, results, _ = Dictionary("stacktitan.com", "autodiscover", "", "8.8.8.8")
+	if len(results) < 1 {
+		t.Fatal("Dictionary did not return any results")
+	}
+	if results[0].IP != "184.106.31.93" {
+		t.Error("Dictionary returned incorrect or non-existent IP Address")
+	}
+	if results[0].Hostname != "autodiscover.stacktitan.com" {
+		t.Error("Dictionary returned incorrect hostname")
+	}
+	if results[0].Source != "Dictionary-CNAME" {
 		t.Error("Dictionary returned incorrect source")
 	}
 }
