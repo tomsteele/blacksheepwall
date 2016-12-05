7a99cc514016c7c9e544c9d65e57ff6098c8497d
Tom Steele <thomasjsteele@gmail.com>
2016-12-05 13:19:47 -0700

    validate domain length and improve domain parsing to account for subdomain usuage

--- bsw/yandex.go
+++ bsw/yandex.go
@@ -1,6 +1,7 @@
 package bsw
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"strings"
@@ -13,8 +14,18 @@ import (
 func YandexAPI(domain, apiURL, serverAddr string) *Tsk {
 	t := newTsk("yandex API")
 	xmlTemplate := "<?xml version='1.0' encoding='UTF-8'?><request><query>%s</query><sortby>rlv</sortby><maxpassages>1</maxpassages><page>0</page><groupings><groupby attr=\" \" mode=\"flat\" groups-on-page=\"100\" docs-in-group=\"1\" /></groupings></request>"
+
+	// Split the domain and reverse the order, then rejoin it for the query.
 	parts := strings.Split(domain, ".")
-	var query = "rhost:" + parts[1] + "." + parts[0] + ".*"
+	if len(parts) < 2 {
+		t.SetErr(errors.New("Invalid domain"))
+		return t
+	}
+	for i, j := 0, len(parts)-1; i < j; i, j = i+1, j-1 {
+		parts[i], parts[j] = parts[j], parts[i]
+	}
+	query := "rhost:" + strings.Join(parts, ".") + ".*"
+
 	postBody := fmt.Sprintf(xmlTemplate, query)
 	resp, err := http.Post(apiURL, "text/xml", strings.NewReader(postBody))
 	if err != nil {
